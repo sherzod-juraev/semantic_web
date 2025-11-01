a8623d2aba09410731987cf90d847bee11755b47
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-11-01 23:33:55 +0500

    main file updated

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from core import register_exception_handler, settings
 
+# import api_router
+from modules import api_router
 
 app = FastAPI()
+
+# connected api_router
+app.include_router(api_router)
+
+app.middleware(
+    CORSMiddleware,
+    allow_origins=[settings.frontend_api_url],
+    allow_credentials=True,
+    allow_methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allow_headers=['*']
+)
+
+register_exception_handler(app)
